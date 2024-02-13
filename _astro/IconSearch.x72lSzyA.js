04ab0aff5b565d17a435d56b0d47d561f3345a2a
D-Sketon <2055272094@qq.com>
2024-02-13 21:08:02 +0800

    first demo

---
+++ _astro/IconSearch.x72lSzyA.js
@@ -0,0 +1,1 @@
+import{c as a}from"./createReactComponent.-3c4MfBK.js";var c=a("search","IconSearch",[["path",{d:"M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0",key:"svg-0"}],["path",{d:"M21 21l-6 -6",key:"svg-1"}]]);export{c as I};
--- src/utils/randomCover.ts
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-import yaml from "js-yaml";
-
-const covers = yaml.load(fs.readFileSync("src/covers.yml", "utf8")) as string[];
-const length = covers.length;
-
-export default () => covers[Math.floor(Math.random() * length)];
