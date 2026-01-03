19fdcdd73fa029ad6f1da0495bc1111ce436c7bf
D-Sketon <2055272094@qq.com>
2026-01-03 19:20:12 +0800

    feat: add TocItem component for improved table of contents rendering

---
+++ src/components/sidebar/TocItem.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type Heading = {
+  depth: number;
+  slug: string;
+  text: string;
+  children: Heading[];
+};
+
+interface TocItemProps {
+  item: Heading;
+  index?: number;
+  key?: string;
+}
+
+const TocItem: React.FC<TocItemProps> = ({ item, index, key }) => {
+  const hasChildren = item.children && item.children.length > 0;
+  const className = `toc-item toc-level-${item.depth}`;
+
+  return (
+    <li className={className} key={key}>
+      <a className="toc-link" href={`#${item.slug}`}>
+        <span className="toc-text">{item.text}</span>
+      </a>
+      {hasChildren && (
+        <ol className="toc-child">
+          {item.children.map((child, childIndex) => (
+            <TocItem key={`${child.slug}-${childIndex}`} item={child} index={childIndex} />
+          ))}
+        </ol>
+      )}
+    </li>
+  );
+};
+
+export default TocItem;
