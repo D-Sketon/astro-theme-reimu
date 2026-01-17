a038eaf449f7914ad716454031c02144ba654d38
D-Sketon <2055272094@qq.com>
2026-01-17 20:39:57 +0800

    fix: page should sort by pubDate

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,11 +5,12 @@ import { SITE, BASE_URL } from "../utils/config";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
+  const sortedPosts = posts.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
   return rss({
     title: SITE.title,
     description: SITE.description,
     site: context.site,
-    items: posts.map((post) => ({
+    items: sortedPosts.map((post) => ({
       ...post.data,
       link: `${BASE_URL}/blog/${post.slug}/`,
     })),
